c30930b22e6dd84aa89d545a8aa413278021d634
a <a@192.168.2.143>
2024-06-15 00:26:25 +0700

    create homepage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
-const handlebars = require("express-handlebars");
+const handlebars = require("express-handlebars").engine;
 const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static( path.join(__dirname,'public')));
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars({
